cd1793c8e01e29d19b0ca8f5d1a137e575feda7a
al fahim bin faruk <fmd03700@gmail.com>
2022-08-12 17:40:07 +0600

    class based views added

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.bookList,name="book-list"),
-    path('details/<str:pk>/', views.getBook,name="get-book"),
-    path('add/', views.addBook,name="add-book"),
-    path('update/<str:pk>/', views.updateBook,name="update-book"),
-    path('delete/<str:pk>/', views.deleteBook,name="delete-book"),
+    path('', views.BookList.as_view(),name="book-list"),
+    path('details/<str:pk>/', views.GetBook.as_view(),name="get-book"),
+    path('add/', views.AddBook.as_view(),name="add-book"),
+    path('update/<str:pk>/', views.UpdateBook.as_view(),name="update-book"),
+    path('delete/<str:pk>/', views.DeleteBook.as_view(),name="delete-book"),
 ]
--- base/views.py
+++ base/views.py
@@ -2,42 +2,84 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.views import APIView
+
+# @api_view(['GET'])
+# def bookList(request):
+#     if request.method == "GET":
+#         bookList = Book.objects.all()
+#         serializedBookList=BookSerializer(bookList,many=True)
+#         return Response(serializedBookList.data)
 
 
-@api_view(['GET'])
-def bookList(request):
-    if request.method == "GET":
+# @api_view(['GET'])
+# def getBook(request, pk):
+#     if request.method == "GET":
+#         book = Book.objects.get(id=pk)
+#         serializedBook=BookSerializer(book,many=False)
+#         return Response(serializedBook.data)
+
+
+# @api_view(['POST'])
+# def addBook(request):
+#     if request.method == "POST":
+#         newBookSerializer=BookSerializer(data=request.data)
+#         if newBookSerializer.is_valid():
+#             newBookSerializer.save()
+#             return Response(newBookSerializer.data)
+#         else:
+#             return Response(newBookSerializer.errors)
+
+
+# @api_view(['PATCH'])
+# def updateBook(request,pk):
+#     if request.method == "PATCH":
+#         book=Book.objects.get(id=pk)
+
+#         serializer=BookSerializer(book,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
+
+# @api_view(['DELETE'])
+# def deleteBook(request,pk):
+#     if request.method == "DELETE":
+#         book=Book.objects.get(id=pk)
+#         book.delete()
+#         return Response({"deleted":pk})
+
+
+class BookList(APIView):
+    def get(self, request):
         bookList = Book.objects.all()
-        serializedBookList=BookSerializer(bookList,many=True)
+        serializedBookList = BookSerializer(bookList, many=True)
         return Response(serializedBookList.data)
 
 
-@api_view(['GET'])
-def getBook(request, pk):
-    if request.method == "GET":
+class GetBook(APIView):
+    def get(self, request, pk):
         book = Book.objects.get(id=pk)
-        serializedBook=BookSerializer(book,many=False)
+        serializedBook = BookSerializer(book, many=False)
         return Response(serializedBook.data)
 
 
-@api_view(['POST'])
-def addBook(request):
-    if request.method == "POST":
-        newBookSerializer=BookSerializer(data=request.data)
+class AddBook(APIView):
+    def post(self, request):
+        newBookSerializer = BookSerializer(data=request.data)
         if newBookSerializer.is_valid():
             newBookSerializer.save()
             return Response(newBookSerializer.data)
         else:
             return Response(newBookSerializer.errors)
-   
 
 
-@api_view(['PATCH'])
-def updateBook(request,pk):
-    if request.method == "PATCH":
-        book=Book.objects.get(id=pk)
-
-        serializer=BookSerializer(book,data=request.data)
+class UpdateBook(APIView):
+    def patch(self, request, pk):
+        book = Book.objects.get(id=pk)
+        serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -45,12 +87,13 @@ def updateBook(request,pk):
             return Response(serializer.errors)
 
 
-@api_view(['DELETE'])
-def deleteBook(request,pk):
-    if request.method == "DELETE":
-        book=Book.objects.get(id=pk)
+class DeleteBook(APIView):
+    def delete(self, request, pk):
+        book = Book.objects.get(id=pk)
         book.delete()
-        return Response({"deleted":pk})
+        return Response({"deleted": pk})
+
+
 # {
 # "title" : "harry",
 # "description" : "harry porter"
